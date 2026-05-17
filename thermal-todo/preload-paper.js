f5c3b2cc332ba1b586b69627c999830590849fc0
davsgladden-dev <davsgladden@gmail.com>
2026-05-17 09:56:58 -0400

    Delete thermal-todo directory

--- thermal-todo/preload-paper.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('paperAPI', {
-    onData:               (cb) => ipcRenderer.on('paper:data',   (_e, data) => cb(data)),
-    torn:                 (data) => ipcRenderer.send('paper:torn', data),
-    paperReady:           ()     => ipcRenderer.send('paper:ready'),
-    setIgnoreMouseEvents: (ignore) => ipcRenderer.send('paper:set-ignore-mouse', { ignore }),
-});
